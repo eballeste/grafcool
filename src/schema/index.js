02e146f741b44c61d2860ecb9323d491d22bfaca
Enrique Balleste <eballeste@divisionof.com>
2017-12-26 13:22:01 -0500

    new schema + query update
    make pizza that can have several toppings

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,14 +2,33 @@ import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './resolvers';
 
 const typeDefs = `
-  type Link {
-    id: ID!,
-    url: String!,
-    description: String!
+  type Maker {
+    id: Int!,
+    name: String!,
+    email: String!,
+    password: String!,
+    pizzas: [Pizza]
+  }
+
+  type Pizza {
+    id: Int!,
+    handle: String!,
+    description: String!,
+    views: Int!,
+    toppings: [Topping],
+    maker: Maker
+  }
+
+  type Topping {
+    id: Int!,
+    name: String!,
+    price: Float!
   }
 
   type Query {
-    allLinks: [Link!]!
+    allPizzas: [Pizza!]!
+    allToppings: [Topping!]!
+    allMakers: [Maker!]!
   }
 
   type Mutation {
