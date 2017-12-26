d6d2203a27b57f2d3f61ed3ac57a624b6b08de38
Enrique Balleste <eballeste@divisionof.com>
2017-12-26 13:45:47 -0500

    config file + node_env
    import db info from config file
    assign node_env=dev on starter script

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
-import schema from './schema';
+import config from 'config';
+const { NODE_ENV } = process.env;
+const configData = config(NODE_ENV);
+const { APIPort } = configData;
+
+import schema from 'schema';
 
 const app = express();
-const PORT = 3000;
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
-app.listen(PORT, ()=> {
-  console.log(`Running on port ${PORT}.`);
+app.listen(APIPort, ()=> {
+  console.log(`Running on port ${APIPort}.`);
 });
